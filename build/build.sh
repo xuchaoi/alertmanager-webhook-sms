e1a8dd0aee05c95fe3c4b432a3c07a676cfbbe25
xuchaoi <1141954686@qq.com>
2020-09-16 21:29:35 +0800

    sms bug fix; wechat test...

---
+++ build/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ../cmd/sms-sender || exit
+
+rm -rf sms-sender
+
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo
---
+++ build/image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf sms-sender
+mv ../cmd/sms-sender/sms-sender .
+chmod +x sms-sender
+docker build --no-cache -t k8s-deploy/sms-sender:v1.0 .
+rm -rf sms-sender
