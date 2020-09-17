1b61414fb8fc8f2ba910d458e6b74d5458192131
zheng199512 <911409649@qq.com>
2020-09-17 15:35:20 +0800

    support few num

--- pkg/webhook/handle/sms-handle.go
+++ pkg/webhook/handle/sms-handle.go
@@ -9,6 +9,7 @@ import (
 	"github.com/xuchaoi/alertmanager-webhook-sms/pkg/util"
 	"io/ioutil"
 	"k8s.io/klog"
+	"strings"
 )
 
 type SMSParams struct {
@@ -25,61 +26,66 @@ type SMSParam struct {
 }
 
 func SMSHandle(smsContent string, smsCfg option.SMSConfiguration) (string, error) {
-	smsParams := SMSParams{
-		Params: SMSParam{
-			SmsCode: smsCfg.Code,
-			SmsContent: smsContent,
-			NewStaffId: smsCfg.NewStaffId,
-			EffectiveDate: smsCfg.EffectiveDate,
-			SubPort: smsCfg.SubPort,
-			CrmpfPubInfo: smsCfg.CrmpfPubInfo,
-		},
-	}
+	var phoneNum []string
+	phoneNum = strings.Split(smsCfg.Code,",")
+	for i := 0;i < len(phoneNum); i++{
+		smsParams := SMSParams{
+			Params: SMSParam{
+				SmsCode: phoneNum[i],
+				SmsContent: smsContent,
+				NewStaffId: smsCfg.NewStaffId,
+				EffectiveDate: smsCfg.EffectiveDate,
+				SubPort: smsCfg.SubPort,
+				CrmpfPubInfo: smsCfg.CrmpfPubInfo,
+			},
+		}
 
-	buf, err := json.Marshal(smsParams)
-	if err != nil {
-		e := fmt.Sprintf("Failed to convert the smsParams to byte through json tool, err: %v", err)
-		return "", errors.New(e)
-	}
-	klog.V(4).Infof("SMS sender request body: %s", string(buf))
-	res, err := util.SMSHttpPost(smsCfg.Url, bytes.NewReader(buf))
-	if err != nil {
-		e := fmt.Sprintf("Failed to send SMS by SMS API, err: %v", err)
-		return "", errors.New(e)
-	}
+		buf, err := json.Marshal(smsParams)
+		if err != nil {
+			e := fmt.Sprintf("Failed to convert the smsParams to byte through json tool, err: %v", err)
+			return "", errors.New(e)
+		}
+		klog.V(4).Infof("SMS sender request body: %s", string(buf))
+		res, err := util.SMSHttpPost(smsCfg.Url, bytes.NewReader(buf))
+		if err != nil {
+			e := fmt.Sprintf("Failed to send SMS by SMS API, err: %v", err)
+			return "", errors.New(e)
+		}
 
-	if res.Body == nil {
-		e := fmt.Sprintf("Failed to get res body which SMS interface returns, smsContent: %s", smsContent)
-		return "", errors.New(e)
-	}
+		if res.Body == nil {
+			e := fmt.Sprintf("Failed to get res body which SMS interface returns, smsContent: %s", smsContent)
+			return "", errors.New(e)
+		}
 
-	resBody, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		e := fmt.Sprintf("Failed to convert SMS interface to return body, err: %v", err)
-		return "", errors.New(e)
-	}
+		resBody, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			e := fmt.Sprintf("Failed to convert SMS interface to return body, err: %v", err)
+			return "", errors.New(e)
+		}
 
-	var f interface{}
-	err = json.Unmarshal(resBody, &f)
-	if err != nil {
-		e := fmt.Sprintf("Failed to convert SMS interface to return body, err: %v", err)
-		return "", errors.New(e)
-	}
+		var f interface{}
+		err = json.Unmarshal(resBody, &f)
+		if err != nil {
+			e := fmt.Sprintf("Failed to convert SMS interface to return body, err: %v", err)
+			return "", errors.New(e)
+		}
 
-	resData := f.(map[string]interface{})
-	klog.V(4).Infof("SMS interface resCode: %d", res.StatusCode)
-	klog.V(4).Infof("SMS interface resData: %v", resData)
-	if resData["object"] == nil {
-		e := fmt.Sprint("Failed to get SMS body.object, err: body.object is nil")
-		return "", errors.New(e)
+		resData := f.(map[string]interface{})
+		klog.V(4).Infof("SMS interface resCode: %d", res.StatusCode)
+		klog.V(4).Infof("SMS interface resData: %v", resData)
+		if resData["object"] == nil {
+			e := fmt.Sprint("Failed to get SMS body.object, err: body.object is nil")
+			return "", errors.New(e)
+		}
+		resDataObj := resData["object"].(map[string]interface{})
+		// Success
+		if resDataObj["respCode"] == "0" {
+			info := fmt.Sprintf("Successed to send SMS interface, response code: 0, desc: %s", resDataObj["respDesc"])
+			return info, nil
+		} else {
+			e := fmt.Sprintf("Successed to send SMS interface, response code: %s, desc: %s", resDataObj["respCode"], resDataObj["respDesc"])
+			return "", errors.New(e)
+		}
 	}
-	resDataObj := resData["object"].(map[string]interface{})
-	// Success
-	if resDataObj["respCode"] == "0" {
-		info := fmt.Sprintf("Successed to send SMS interface, response code: 0, desc: %s", resDataObj["respDesc"])
-		return info, nil
-	} else {
-		e := fmt.Sprintf("Successed to send SMS interface, response code: %s, desc: %s", resDataObj["respCode"], resDataObj["respDesc"])
-		return "", errors.New(e)
-	}
+	klog.V(4).Info("send messages: %d",len(phoneNum))
 }
